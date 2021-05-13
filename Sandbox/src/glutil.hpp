ecf47f2f62afd292b704cb90689c2faaa69e7a26
Brendan Gould <brendan.gould314@gmail.com>
2021-05-12 18:13:30 -0600

    Add glm to build

--- Sandbox/src/glutil.hpp
+++ Sandbox/src/glutil.hpp
@@ -7,14 +7,14 @@
 
 // error handling macros
 // -----------------------------------------------------------------------------
-#define ASSERT(x)                                                              \
-	if (!(x)) {                                                                \
-		raise(SIGINT);                                                         \
+#define ASSERT(x)                                                                                  \
+	if (!(x)) {                                                                                    \
+		raise(SIGINT);                                                                             \
 	}
 
-#define GL_CALL(x)                                                             \
-	glClearError();                                                            \
-	x;                                                                         \
+#define GL_CALL(x)                                                                                 \
+	glClearError();                                                                                \
+	x;                                                                                             \
 	ASSERT(glLogCall(#x, __FILE__, __LINE__))
 
 static void glClearError() {
@@ -24,8 +24,8 @@ static void glClearError() {
 
 static bool glLogCall(const char* function, const char* file, int line) {
 	while (GLenum error = glGetError()) {
-		std::cout << "[OpenGL ERROR]: " << error << " in " << function << " in "
-				  << file << ":" << line << std::endl;
+		std::cout << "[OpenGL ERROR]: " << error << " in " << function << " in " << file << ":"
+				  << line << std::endl;
 		return false;
 	}
 
